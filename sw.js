5f014394fe913c280d823cb589b11e80af9d55e7
唐煜 <431585737@qq.com>
2025-11-20 05:16:57 +0800

    Site updated: 2025-11-20 05:16:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1763584412938";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1763586842318";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
